9624e4d0a3e4d160c393e64b389d60864aef4b8f
Abhijeet Verma <jeetabi0241@gmail.com>
2025-12-31 02:19:13 +0530

    fourthcommit

--- script.js
+++ script.js
@@ -39,3 +39,18 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+document.querySelectorAll("#nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
+
